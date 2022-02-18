e1a9cf1dcd0619b80f803f6c4661daa5d288bcef
petro71 <petro,savytsky@gmail.com>
2022-02-18 19:47:52 +0200

    after lection3

--- js/scripts.js
+++ js/scripts.js
@@ -16,5 +16,22 @@ $(document).ready(function(){
             }
         }
     });
+
+    $('.owl-carousel-2').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1.2
+            },
+
+            1000:{
+                items:4
+            }
+        }
+    });
 });
 
